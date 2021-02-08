7ca16cd0786818810b91613f0eb80f3a1389cf54
Marcello Frugnoli <frugnoli@outlook.com>
2021-02-08 08:40:37 -0300

    Create Desafio_072.py

---
+++ Desafio_072.py
@@ -0,0 +1,9 @@
+# Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte. 
+# Seu programa deverá ler um número pelo teclado (entre 0 e 10) e mostrá-lo por extenso.
+t = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
+while True:
+    num = int(input("Digite um número entre 0 e 10: "))
+    if 0 <= num <= 20:
+        break
+    print("Tente novamente. ", end='')
+print(f"Você digitou o número {t[num]}.")
