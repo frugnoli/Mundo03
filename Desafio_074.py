467cace362e2cb9d91a3201ca296cd2e503b9c3f
Marcello Frugnoli <frugnoli@outlook.com>
2021-02-15 21:33:45 -0300

    Update Desafio_074.py

--- Desafio_074.py
+++ Desafio_074.py
@@ -1,1 +1,9 @@
 # Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+from random import randint
+num = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+print(f"Os números sorteados foram: ", end="")
+for x in num:
+    print(f"{x} ", end="")
+print(f"\nO maior valor sorteado foi {max(num)}")
+print(f"O menor valor sorteado foi {min(num)}")
+
