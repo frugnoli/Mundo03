1b18be6575b1601175f013982d1094d384ffab27
Marcello Frugnoli <frugnoli@outlook.com>
2021-02-15 23:27:38 -0300

    Update Desafio_075.py

--- Desafio_075.py
+++ Desafio_075.py
@@ -3,3 +3,17 @@
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
 
+num = (int(input("Digite um número: ")),
+int(input("Digite outro número: ")),
+int(input("Digite mais um número: ")),
+int(input("Digite o último número: ")))
+print(f"Você digitou os valores {num}")
+print(f"O valor 9 apareceu {num.count(9)}")
+if 3 in num:
+    print(f"O valor 3 apareceu {num.index(3)+1}ª posição")
+else:
+    print("O valor 3 não foi digitado")
+print(f"Os valores pares digitados foram ", end='')
+for n in num:
+    if n % 2 == 0:
+        print(n, end=' ')
